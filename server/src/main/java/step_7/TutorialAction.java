a033a58a037cbab9156996775ef95560eb2e02d6
Dierk Koenig <dierk.koenig@canoo.com>
2013-03-02 23:46:46 +0100

    go from CommandHandler to DolphinServerAction

--- server/src/main/java/step_7/TutorialAction.java
+++ server/src/main/java/step_7/TutorialAction.java
@@ -3,23 +3,25 @@ package step_7;
 import org.opendolphin.core.comm.Command;
 import org.opendolphin.core.comm.NamedCommand;
 import org.opendolphin.core.server.ServerDolphin;
+import org.opendolphin.core.server.action.DolphinServerAction;
+import org.opendolphin.core.server.comm.ActionRegistry;
+import org.opendolphin.core.server.comm.CommandHandler;
 import org.opendolphin.core.server.comm.NamedCommandHandler;
+import step_5.TutorialConstants;
 
 import java.util.List;
 
+import static step_5.TutorialConstants.CMD_LOG;
 import static step_6.TutorialConstants.ATT_FIRSTNAME;
 import static step_6.TutorialConstants.PM_PERSON;
 
-public class TutorialAction implements NamedCommandHandler {
-    private final ServerDolphin serverDolphin;
+public class TutorialAction extends DolphinServerAction {
 
-    public TutorialAction(ServerDolphin serverDolphin) {
-        this.serverDolphin = serverDolphin;
-    }
-
-    @Override
-    public void handleCommand(NamedCommand command, List<Command> response) {
-        System.out.println(serverDolphin.getAt(PM_PERSON).getAt(ATT_FIRSTNAME).getValue() +" has been saved.");
-
+    public void registerIn(ActionRegistry actionRegistry) {
+        actionRegistry.register(CMD_LOG, new CommandHandler<Command>() {
+            public void handleCommand(Command command, List<Command> response) {
+                System.out.println(getServerDolphin().getAt(TutorialConstants.PM_PERSON).getAt(TutorialConstants.ATT_FIRSTNAME).getValue());
+            }
+        });
     }
 }
