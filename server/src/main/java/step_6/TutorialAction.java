58879fd79ba759b251e3401b5836e5bd35659d2e
detlef-brendle <detlef.brendle@canoo.com>
2013-03-04 10:38:46 +0100

    fix step 6 sample

--- server/src/main/java/step_6/TutorialAction.java
+++ server/src/main/java/step_6/TutorialAction.java
@@ -1,18 +1,12 @@
 package step_6;
 
 import org.opendolphin.core.comm.Command;
-import org.opendolphin.core.comm.NamedCommand;
-import org.opendolphin.core.server.ServerDolphin;
 import org.opendolphin.core.server.action.DolphinServerAction;
 import org.opendolphin.core.server.comm.ActionRegistry;
 import org.opendolphin.core.server.comm.CommandHandler;
-import org.opendolphin.core.server.comm.NamedCommandHandler;
 
 import java.util.List;
 
-import static step_5.TutorialConstants.ATT_FIRSTNAME;
-import static step_5.TutorialConstants.CMD_LOG;
-import static step_5.TutorialConstants.PM_PERSON;
 import static step_6.TutorialConstants.*;
 
 public class TutorialAction extends DolphinServerAction {
